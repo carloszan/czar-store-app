c6d87e855ec9f12b10f2dc6a35bbca3b2ad353a2
Carlos <carloshlzansavio@gmail.com>
2022-08-16 18:43:57 +0100

    feat: add carousel

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,3 +1,10 @@
+import Image from "next/image";
+import { Carousel } from "../ui/carousel";
+
 export default function Products() {
-  return <p>Products</p>;
+  return (
+    <div className="w-screen flex justify-center">
+      <Carousel />
+    </div>
+  );
 }
